4369591b93dcb19a1ad19a0c3e5db3a6cc5b33fa
Osmary <osmaribarriga@gmail.com>
2024-07-31 22:44:42 -0400

    multitenancy

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RolePermissionProvider::class,
+    App\Providers\TenancyServiceProvider::class,
 ];
---
+++ routes/api/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\InventarioController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\RoleController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+Route::get('/users', [RegisterController::class, 'index'])->name('user.index');
+ 
+Route::middleware(['auth:sanctum'])->group(function () {
+  Route::post('/registro', [RegisterController::class, 'store'])->name('user.register');
+  Route::get('/profile', [AuthenticatedSessionController::class, 'profile'])->name('profile');
+  Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+  Route::apiResource('/inventario', InventarioController::class)->only('index', 'store', 'update', 'destroy');
+  Route::group(['middleware' => ['role:sadmin']], function () {
+    Route::get('/inventario/{id}', [InventarioController::class, 'show']);
+  });
+  Route::apiResource('/permission', PermissionController::class);
+  Route::apiResource('/role', RoleController::class);
+});
+
+
+
